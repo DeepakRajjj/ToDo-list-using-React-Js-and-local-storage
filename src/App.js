c1a7ddeda2ad0b5779dba94f17b6587988498d5d
Deepak Raj <deepakraj04112001@gmail.com>
2024-04-14 20:11:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AddTodo from './Components/AddTodo';
+import TodoList from './Components/AddTodo/TodoList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col gap-y-2 mx-auto mt-10 px-4 md:px-20 p-5 max-w-screen-lg bg-gray-100'>
+      <h1 className='text-2xl font-bold mb-4 text-center'>Todo List</h1>
+      <AddTodo />
+      <TodoList />
     </div>
   );
-}
+};
 
 export default App;
